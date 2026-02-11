45ec1356700d8e33a9e3c8437de3a9e9378dd286
Carlos Deotti <deotti@gmail.com>
2026-02-11 15:04:39 -0300

    copilot mais atrapalhando que ajudando

--- Model/NameSanitizer.php
+++ Model/NameSanitizer.php
@@ -12,78 +12,23 @@
 
 namespace AdDeo\CustomerNameSanitaizer\Model;
 
-use Magento\Framework\Model\AbstractModel;
-use Magento\Framework\DataObject\IdentityInterface;
-use Magento\Framework\Model\Context;
-use Magento\Framework\Registry;
-use Magento\Framework\Model\ResourceModel\AbstractResource;
-use Magento\Framework\Data\Collection\AbstractDb;
-
 /**
  * This class is responsible for sanitizing customer names.
  */
-class NameSanitizer extends AbstractModel implements IdentityInterface
+class NameSanitizer
 {
-    const CACHE_TAG = 'addeo_customernamesanitaizer_name_sanitizer';
-
     /**
-     * Model cache tag for clear cache in after save and after delete
+     * Sanitizes the given first name by removing all whitespace.
      *
-     * @var string
-     */
-    protected $_cacheTag = self::CACHE_TAG;
-
-    /**
-     * Prefix of model events names
+     * @param string|null $firstName
      *
-     * @var string
+     * @return string|null
      */
-    protected $_eventPrefix = 'name_sanitizer';
-
-    /**
-     * Constructor
-     *
-     * @param Context $context
-     * @param Registry $registry
-     * @param AbstractResource $resource
-     * @param AbstractDb $resourceCollection
-     * @param array $data
-     */
-    public function __construct(
-        Context $context,
-        Registry $registry,
-        AbstractResource $resource = null,
-        AbstractDb $resourceCollection = null,
-        array $data = []
-    ) {
-        parent::__construct(
-            $context,
-            $registry,
-            $resource,
-            $resourceCollection,
-            $data
-        );
-    }
-
-    /**
-     * Initialize resource model
-     *
-     * @return void
-     */
-    protected function _construct()
+    public function sanitize(?string $firstName): ?string
     {
-        $this->_init(
-            'AdDeo\CustomerNameSanitaizer\Model\ResourceModel\NameSanitizer'
-        );
-    }
-
-    /**
-     * Return a unique id for the model.
-     *
-     * @return array
-     */
-    public function getIdentities()
-    {
-        return [self::CACHE_TAG . '_' . $this->getId()];
+        if ($firstName === null) {
+            return null;
+        }
+        return preg_replace('/\s+/', '', $firstName);
     }
 }
